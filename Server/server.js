b387be93db854fa92daad49a3934cdbf407dbe36
ColumbiaCoding <gabriel.regus@gmail.com>
2023-11-15 18:48:21 -0500

    adding server.js code

--- Server/server.js
+++ Server/server.js
@@ -29,6 +29,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(routes)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+}
+
 // app.use('/transactions', transactionRoutes);
 // app.use('/users', userRoutes);
 
