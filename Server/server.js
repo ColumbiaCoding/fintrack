c434371cc64e85cb061ab2139749c0894240b6a3
ColumbiaCoding <gabriel.regus@gmail.com>
2023-11-15 19:02:05 -0500

    adding heroku

--- Server/server.js
+++ Server/server.js
@@ -31,10 +31,10 @@ app.use(cors());
 app.use(routes)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(__dirname, '../Client/dist')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../Client/dist/index.html'));
   });
 }
 
